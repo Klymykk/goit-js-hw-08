a9efff318bba1b12e427338ebd2a5dd270e86e28
Klymuk <114335106+Klymykk@users.noreply.github.com>
2023-05-05 22:46:19 +0300

    commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(function(data) {
+    localStorage.setItem(STORAGE_KEY, data.seconds);
+},1000));
+
+player.setCurrentTime(Number(localStorage.getItem(STORAGE_KEY)));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector(".feedback-form");
+const LOCAL_KEY = "feedback-form-state";
+const formInput = {};
 
+form.addEventListener("input", throttle(onInput),500);
+form.addEventListener("submit", onSubmit)
+
+function onInput(evt) {
+    formInput[evt.target.name] = evt.target.value;
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(formInput));
+};
+
+function onSubmit(evt) {
+    evt.preventDefault();
+    console.log(formInput);
+    localStorage.removeItem(LOCAL_KEY);
+    evt.target.reset()
+};
+
+function checkLocal(){
+    const savedSettings = JSON.parse(localStorage.getItem(LOCAL_KEY));
+    if(savedSettings){
+        const {email, message} = savedSettings;
+        form.email.value = email||'';
+        form.message.value = message||'';
+    }
+}
+checkLocal()
